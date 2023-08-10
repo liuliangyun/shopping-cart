54a5a1e8fcde4d1d7b33a51b833e86d46a0b8e14
lyliu <lyliu@thoughtworks.com>
2023-08-10 10:48:18 +0800

    add eslint to check code style

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "es2021": true,
+    "jest": true,
+  },
+  "extends": [
+    "eslint:recommended",
+    "plugin:react/recommended"
+  ],
+  "overrides": [
+    {
+      "env": {
+        "node": true
+      },
+      "files": [
+        ".eslintrc.{js,cjs}"
+      ],
+      "parserOptions": {
+        "sourceType": "script"
+      }
+    }
+  ],
+  "parserOptions": {
+    "ecmaVersion": "latest",
+    "sourceType": "module"
+  },
+  "plugins": [
+    "react"
+  ],
+  "settings": {
+    "react": {
+      "version": "detect"
+    }
+  },
+  "rules": {
+    "react/react-in-jsx-scope": "off",
+  }
+}
+
